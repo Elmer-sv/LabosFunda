264b46e5208ea622ad5eec3aa12428be7555648f
Elmer Roman Chavarria Salazar <Elmer-sv>
2020-05-06 15:26:56 -0600

    T1E1

---
+++ T1E1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1,num2,num3;
+    cin>>num1>>num2>>num3;
+
+    int suma=(num1+num2+num3)/3;
+
+    cout<< "El resultado de la suma es:" << suma;
+
+    return  0;
+}
---
+++ ejemplo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "veremos dijo el ciego" << endl;
+    
+    return 0;
+}
